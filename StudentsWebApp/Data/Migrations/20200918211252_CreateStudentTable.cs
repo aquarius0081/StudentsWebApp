ae3c1251c4080d95fde5cd542fca86e64b000708
Rinat Yusupov <rinat.yusupov1@outlook.com>
2020-09-19 01:00:57 +0300

    Updated DB name in appsettings. Added migration for creation of Student table.

---
+++ StudentsWebApp/Data/Migrations/20200918211252_CreateStudentTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentsWebApp.Data.Migrations
+{
+    public partial class CreateStudentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                    .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                    .Annotation("SqlServer:Autoincrement", true),
+                    Sex = table.Column<string>(maxLength: 6, nullable: false),
+                    LastName = table.Column<string>(maxLength: 40, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 40, nullable: false),
+                    MiddleName = table.Column<string>(maxLength: 60, nullable: true),
+                    Pseudonym = table.Column<string>(maxLength: 16, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "PseudonymUniqueIndex",
+                table: "Student",
+                column: "Pseudonym",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Student");
+        }
+    }
+}
